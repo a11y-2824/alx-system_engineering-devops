8273163198e1e683213a69e10d1275f7aa1c80e4
a11y-2824 <wachana.anne@gmail.com>
2023-01-12 05:21:22 +0300

    install a package

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,10 @@
+# Install a package
+
+exec {'pip3 install flask==2.1.0':
+  command => '/usr/bin/pip3 install flask==2.1.0'
+}
+
+package {'flask':
+  ensure  => 'installed';
+  require => Exec['pip3 install flask==2.1.0'
+}
