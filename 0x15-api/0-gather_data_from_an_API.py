9cd79877a0e422fd904956bc405c386c87e7e05f
a11y-2824 <wachana.anne@gmail.com>
2023-08-08 19:37:00 +0300

    -fixed pycodestyle, made file executable

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Python script that, using this REST API, for a given employee ID, 
+Python script that, using this REST API, for a given employee ID
 returns information about his/her TODO list progress.
 """
 import requests
@@ -9,14 +9,16 @@ from sys import argv
 URL = "https://jsonplaceholder.typicode.com/"
 
 if __name__ == "__main__":
-    if len(argv)>1:
-        if argv[1].isdecimal() and int(argv[1])>=0:
+    if len(argv) > 1:
+        if argv[1].isdecimal() and int(argv[1]) >= 0:
             employeeId = int(argv[1])
-            user_resp = requests.get("{}/users/{}".format(URL, employeeId)).json()
-            todo_resp = requests.get("{}/todo".formart(URL))
+            user_resp = requests.get("{}/users/{}".format(
+                                     URL, employeeId)).json()
+            todo_resp = requests.get("{}/todo".format(URL))
             employeeName = user_resp.get('name')
-            TotalNumberOfTasks = todo_resp.get('tasks')
-            NumberOfDoneTasks = (t for t in TotalNumberOfTasks if t.get('Completed'))
+            TotalNumberOfTasks = (t for t in todo_resp if t.get('tasks'))
+            NumberOfDoneTasks = (t for t in TotalNumberOfTasks if t.get(
+                                 'Completed'))
             print("Employee {} is done with tasks({}/{}):".format(
                 employeeName, len(NumberOfDoneTasks), len(TotalNumberOfTasks)
             ))
